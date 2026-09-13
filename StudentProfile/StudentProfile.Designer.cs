4853325bda19a68bd340f1a40d7384792df97757
cedrickbaon <cedrickbaon0711@gmail.com>
2026-09-13 23:51:46 +0800

    Add student search

--- StudentProfile/StudentProfile.Designer.cs
+++ StudentProfile/StudentProfile.Designer.cs
@@ -30,32 +30,41 @@
         {
             lblStudProf = new Label();
             btnShow = new Button();
+            textBox1 = new TextBox();
             SuspendLayout();
             // 
             // lblStudProf
             // 
             lblStudProf.AutoSize = true;
-            lblStudProf.Location = new Point(285, 110);
+            lblStudProf.Location = new Point(186, 109);
             lblStudProf.Name = "lblStudProf";
-            lblStudProf.Size = new Size(161, 15);
+            lblStudProf.Size = new Size(146, 15);
             lblStudProf.TabIndex = 0;
-            lblStudProf.Text = "Student Profile: Cedrick Baon";
+            lblStudProf.Text = "Student Name/Student ID:";
             // 
             // btnShow
             // 
-            btnShow.Location = new Point(331, 166);
+            btnShow.Location = new Point(316, 177);
             btnShow.Name = "btnShow";
             btnShow.Size = new Size(75, 23);
             btnShow.TabIndex = 1;
-            btnShow.Text = "Show";
+            btnShow.Text = "Search";
             btnShow.UseVisualStyleBackColor = true;
             btnShow.Click += btnShow_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(358, 106);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
             // 
             // StudentProfile
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
             Controls.Add(btnShow);
             Controls.Add(lblStudProf);
             Name = "StudentProfile";
@@ -68,5 +77,6 @@
 
         private Label lblStudProf;
         private Button btnShow;
+        private TextBox textBox1;
     }
 }
