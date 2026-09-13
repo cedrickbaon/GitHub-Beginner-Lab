4479569150e0f035aaadb670f6aba49ef2351041
cedrickbaon <cedrickbaon0711@gmail.com>
2026-09-13 19:27:30 +0800

    Add StudentProfile Visual Studio project

---
+++ StudentProfile/StudentProfile.cs
@@ -0,0 +1,15 @@
+namespace StudentProfile
+{
+    public partial class StudentProfile : Form
+    {
+        public StudentProfile()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Student Profile: Cedrick Baon");
+        }
+    }
+}
